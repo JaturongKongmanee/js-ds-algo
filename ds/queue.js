39111e8b5280029f769232b22cd00fc49a43c285
dillsunnyb11 <dill_sunnyb11@hotmail.com>
2020-08-23 13:35:55 -0600

    Add a queue

---
+++ ds/queue.js
@@ -0,0 +1,32 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(value) {
+    this.queue.push(value);
+  }
+
+  dequeue() {
+    if (this.queue.length === 0) {
+      return undefined;
+    }
+
+    return this.queue.shift();
+  }
+
+  size() {
+    return this.queue.length;
+  }
+
+  isEmpty() {
+    return this.collection.length === 0;
+  }
+}
+
+let myQueue = new Queue();
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+console.log(myQueue.queue);
+console.log(myQueue.dequeue());
+console.log(myQueue.queue);
